3382fc042099c9475caf9a4c2bb3d40fcf761c40
Gabriela Hernandez <gabriela.hernandez78018@gmail.com>
2023-12-24 13:39:28 -0800

    initial changes

---
+++ components/AboutSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// 1. AboutSection is a functional component written using an arrow function
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
